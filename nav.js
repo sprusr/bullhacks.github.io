a9f30750aac4340ce9ec80d075896bf6b2e8838c
Pandelis Zembashis <info@pandelis.me>
2017-02-03 12:14:35 +0000

    FAQ section has been populated

--- nav.js
+++ nav.js
@@ -3,6 +3,7 @@
   var color = 0;
   var colors = ['#465eab', '#e6c622', '#ea473b']
   var x = document.getElementsByClassName('questionBorder')
+  var y = document.getElementsByClassName('questionBackground')
 
 
   for (i = 0; i < x.length; i++) {
@@ -18,4 +19,19 @@
         color = 0;
       }
   }
+
+
+  for (i = 0; i < x.length; i++) {
+      var parent = y[i].parentElement
+      var rotate = Math.floor(Math.random() * 10) - 5 || Math.floor(Math.random() * 10) + 5
+      y[i].style.width = parent.clientWidth + 'px';
+      y[i].style.height = parent.clientHeight + 'px';
+      y[i].style.transform = "rotate("+ rotate +"deg)"
+      color += 1
+      if (color > 2) {
+        color = 0;
+      }
+  }
+
+
 })()
