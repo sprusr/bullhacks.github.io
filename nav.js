b82d0271a5bcae530d0653ab573a591b50f5d538
sprusr <scott@hakke.net>
2017-02-05 12:40:36 +0000

    Fix issue with faq boxes, and tidy up
    Switched some of the js sizing stuff for the faq boxes to css, and
    cleaned up how the apply button was displayed.

--- nav.js
+++ nav.js
@@ -1,6 +1,4 @@
 (function(){
-
-  var color = 0;
   var colors = ['#465eab', '#e6c622', '#ea473b']
   var x = document.getElementsByClassName('questionBorder')
   var y = document.getElementsByClassName('questionBackground')
@@ -8,57 +6,24 @@
 
 
   for (var i = 0; i < x.length; i++) {
-      var parent = x[i].parentElement
-      var rotate = Math.floor(Math.random() * 10) - 5 || Math.floor(Math.random() * 10) + 5
+      var rotate = Math.floor((Math.random() - 0.5) * 15)
       var ranColor = Math.floor(Math.random() * 3)
-      x[i].style.width = parent.clientWidth + 'px';
-      x[i].style.height = parent.clientHeight + 'px';
       x[i].style.borderColor = colors[ranColor];
       x[i].style.transform = "rotate("+ rotate +"deg)"
-      color += 1
-      if (color > 2) {
-        color = 0;
-      }
   }
-
-
-  for (var i = 0; i < y.length; i++) {
-      var parent = y[i].parentElement
-      var rotate = Math.floor(Math.random() * 10) - 5 || Math.floor(Math.random() * 10) + 5
-      y[i].style.width = parent.clientWidth + 'px';
-      y[i].style.height = parent.clientHeight + 'px';
-      y[i].style.transform = "rotate("+ rotate +"deg)"
-      color += 1
-      if (color > 2) {
-        color = 0;
-      }
-  }
-
-  // for (var i=0; i < buttons.length; i++){
-  //   var parent = y[i].parentElement
-  //   y[i].style.width = parent.clientWidth + 'px';
-  //   y[i].style.height= parent.clientHeight + 'px';
-  //   y[i]
-  // }
-
-
-
-
-
 })()
 
 function initMap() {
-      var uluru = {lat: -25.363, lng: 131.044};
-      var map = new google.maps.Map(document.getElementById('location'), {
-
-        draggable: false,
-        scrollwheel: false,
-        panControl: false,
-        zoom: 14,
-        center: {lat: 52.4806704 , lng: -1.8926539},
-      });
-      var marker = new google.maps.Marker({
-        position: {lat:52.4832336 , lng: -1.8843293},
-        map: map
-      });
-    }
+  var uluru = {lat: -25.363, lng: 131.044}
+  var map = new google.maps.Map(document.getElementById('location'), {
+    draggable: false,
+    scrollwheel: false,
+    panControl: false,
+    zoom: 14,
+    center: {lat: 52.4806704 , lng: -1.8926539},
+  })
+  var marker = new google.maps.Marker({
+    position: {lat:52.4832336 , lng: -1.8843293},
+    map: map
+  })
+}
